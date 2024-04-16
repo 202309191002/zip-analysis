ac5e62fd9a140f794b1a50286b7be42e25e9e265
Katherine Jiang <JiangM14@cardiff.ac.uk>
2024-04-16 14:01:58 +0100

    automate analysis

--- bin/countwords.py
+++ bin/countwords.py
@@ -2,11 +2,16 @@
 Count the occurrences of all words in a text
 and write them to a CSV-file.
 """
-
+import argparse
 import string
 from collections import Counter
 
 import utilities as util
+
+def main(args):
+    """Run the command line program."""
+    word_counts = count_words(args.infile)
+    util.collection_to_csv(word_counts, num=args.num)
 
 
 def count_words(reader):
@@ -17,8 +22,18 @@ def count_words(reader):
     word_list = [word.lower() for word in stripped if word]
     word_counts = Counter(word_list)
     return word_counts
+    
 
-
-with open('data/frankenstein.txt', 'r') as reader:
-    word_counts = count_words(reader)
-util.collection_to_csv(word_counts, num=100)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=(
+        "Count the occurrences of all words in a text "
+        "and write them to a CSV-file."
+    ))
+    parser.add_argument('infile', type=argparse.FileType('r'),
+                        nargs='?', default='-',
+                        help='Input file name')
+    parser.add_argument('-n', '--num',
+                        type=int, default=None,
+                        help='Output only n most frequent words')
+    args = parser.parse_args()
+    main(args)
